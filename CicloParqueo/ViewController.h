a008eaa5c06300adffaa4f744df39d6e6a4dd1eb
Andres Otalora Perez <andresotaloraperez@MacBook-Air-de-Andres.local>
2015-05-07 20:56:57 -0500

    Initial Commit

---
+++ CicloParqueo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CicloParqueo
+//
+//  Created by Andres Otalora Perez on 7/05/15.
+//  Copyright (c) 2015 Andres Otalora Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CicloParqueo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CicloParqueo
+//
+//  Created by Andres Otalora Perez on 7/05/15.
+//  Copyright (c) 2015 Andres Otalora Perez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
